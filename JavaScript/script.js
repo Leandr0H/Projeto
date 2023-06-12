125a84e6eb1cd08a4cc427bb6c699c82d7a19ac0
unknown <leandrohdess@gmail.com>
2023-06-12 08:55:09 -0300

    :tada: initial commit

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+function togglemode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    
+    const img = document.querySelector('#profile img')
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', 'Mobile/avatar-light.png')
+    } else {
+        img.setAttribute('src', 'Mobile/avatar-dark.png')
+    }
+}
